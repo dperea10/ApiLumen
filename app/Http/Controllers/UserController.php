46e8fb6ff15a954e370453da482a8f4b6acbca47
Diego Perea <diego.perea.dp@gmail.com>
2020-05-13 16:15:15 -0500

    creacion de api lumen restful con usuario

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use App\User;
+
+class UserController extends Controller
+{
+
+    
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function register(Request $request){
+        
+        //Antes de registrar el usuario valida los datos
+        $rules = [
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+         ];
+
+         $customMessages = [
+            'required' => 'Please fill attribute :attribute'
+       ];
+
+       $this->validate($request, $rules, $customMessages);
+
+       try {
+           $hasher = app()->make('hash');
+           $username = $request->input('username');
+           $email = $request->input('email');
+           $password = $hasher->make($request->input('password'));
+
+           $save = User::create([
+               'username'=> $username,
+               'email'=> $email,
+               'password'=> $password,
+               'api_token'=> ''
+           ]);
+           $res['status'] = true;
+           $res['message'] = 'Registration success!';
+           return response($res, 201);
+
+       } catch (\Illuminate\Database\QueryException $ex) {
+           $res['status'] = false;
+           $res['message'] = $ex->getMessage();
+           return response($res, 500);
+       }
+   }
+
+   public function get_user()
+   {
+       $user = User::all();
+       if ($user) {
+             $res['status'] = true;
+             $res['message'] = $user;
+
+             return response($res);
+       }else{
+         $res['status'] = false;
+         $res['message'] = 'Cannot find user!';
+
+         return response($res);
+       }
+   }
+    
+}
